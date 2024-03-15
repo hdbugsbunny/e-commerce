33639dfe2235c89760ef2a710c6c043e54334fc7
Harshit Durgapal <harshit.durgapal@mobcoder.com>
2024-03-15 19:00:29 +0530

    One-To-Many and Many-To-Many Relationships, Updated Cart Model

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const sequelize = require("./util/database");
 
 const Product = require("./models/product");
 const User = require("./models/user");
+const Cart = require("./models/cart");
+const CartItem = require("./models/cart-item");
 
 const app = express();
 
@@ -43,10 +45,14 @@ app.use(errorController.get404);
 
 Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
 User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
 
 sequelize
-  // .sync({ force: true })
-  .sync()
+  .sync({ force: true })
+  // .sync()
   .then((result) => {
     return User.findByPk(1);
     // console.log("🚀 ~ .then ~ result:", result);
