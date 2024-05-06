147cc7433ede31685f10ac2f5907bb4369f41dad
Harshit Durgapal <harshit.durgapal@mobcoder.com>
2024-05-06 17:52:51 +0530

    Deleting The Cart Items

--- models/user.js
+++ models/user.js
@@ -36,6 +36,15 @@ userSchema.methods.addToCart = function (product) {
   return this.save();
 };
 
+userSchema.methods.deleteItemFromCart = function (productId) {
+  const updatedCartItems = this.cart.items.filter(
+    (item) => item.productId.toString() !== productId.toString()
+  );
+  this.cart.items = updatedCartItems;
+
+  return this.save();
+};
+
 module.exports = mongoose.model("User", userSchema);
 // const mongodb = require("mongodb");
 // const getDb = require("../util/database").getDb;
