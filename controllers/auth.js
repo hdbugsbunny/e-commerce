234f06759f5c30228f7c0dfa30b0f52b42596c70
Harshit Durgapal <harshit.durgapal@mobcoder.com>
2024-05-09 19:34:07 +0530

    Code Adjustment and Making Navigation CSS for Mobile

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,13 +1,10 @@
 const User = require("../models/user");
 
 exports.getLogin = (req, res, next) => {
-  const isAuthenticated =
-    req.get("Cookie")?.split(";")[1]?.trim()?.split("=")[1] === "true" || false;
-  console.log("🚀 ~ session:", req.session);
   res.render("auth/login", {
     docTitle: "Login",
     path: "/login",
-    isAuthenticated,
+    isAuthenticated: false,
   });
 };
 
