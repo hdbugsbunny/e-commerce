c32691719054e2f2118fcf2f438afac81a1e72c6
Harshit Durgapal <harshit.durgapal@mobcoder.com>
2024-05-09 11:48:37 +0530

    Creating The Login Form

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ app.set("views", "views");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
+const authRoutes = require("./routes/auth");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
@@ -38,6 +39,7 @@ app.use((req, res, next) => {
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 //! Universal Error Handler
 app.use(errorController.get404);
