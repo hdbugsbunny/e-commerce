b337b5089b041b324c6d9fb235b5cc1a0a86cfaf
Harshit Durgapal <harshit.durgapal@mobcoder.com>
2024-05-09 14:27:00 +0530

    Using The Session Middleware

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
 exports.getLogin = (req, res, next) => {
   const isAuthenticated =
-    req.get("Cookie").split(";")[1].trim().split("=")[1] === "true";
+    req.get("Cookie")?.split(";")[1]?.trim()?.split("=")[1] === "true" || false;
+  console.log("🚀 ~ session:", req.session);
   res.render("auth/login", {
     docTitle: "Login",
     path: "/login",
@@ -9,6 +10,6 @@ exports.getLogin = (req, res, next) => {
 };
 
 exports.postLogin = (req, res, next) => {
-  res.setHeader("Set-Cookie", "isAuthenticated=true");
+  req.session.isAuthenticated = true;
   res.redirect("/");
 };
