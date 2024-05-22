281b0d1da54e33982951f3c1842ae6f7f0025ba3
Harshit Durgapal <harshit.durgapal@mobcoder.com>
2024-05-22 16:11:37 +0530

    More Validators

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { check } = require("express-validator");
+const { check, body } = require("express-validator");
 
 const authController = require("../controllers/auth");
 
@@ -17,15 +17,23 @@ router.post("/login", authController.postLogin);
 
 router.post(
   "/signup",
-  check("email")
-    .isEmail()
-    .withMessage("Enter Valid Email!")
-    .custom((value, { req }) => {
-      if (value === "test@test.com") {
-        throw new Error("This email is forbidden!");
-      }
-      return true;
-    }),
+  [
+    check("email")
+      .isEmail()
+      .withMessage("Enter Valid Email!")
+      .custom((value, { req }) => {
+        if (value === "test@test.com") {
+          throw new Error("This email is forbidden!");
+        }
+        return true;
+      }),
+    body(
+      "password",
+      "Enter Valid Password With Only Numbers, Text And Atleast 5 Characters"
+    )
+      .isLength({ min: 5 })
+      .isAlphanumeric(),
+  ],
   authController.postSignup
 );
 
